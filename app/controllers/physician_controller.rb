391751ead6a6b2759c9610a822f91009aab28e2c
Regina Ranstrom <reginaranstrom@gmail.com>
2016-02-20 16:44:30 -0500

    revamped header and added trackers

--- app/controllers/physician_controller.rb
+++ app/controllers/physician_controller.rb
@@ -1,6 +1,8 @@
 class PhysicianController < ApplicationController
 	def index
 		@draft_orders = Order.where(status: "draft")
+		@active_orders = Order.where(status: "active")
+		@approved_orders = Order.where(status: "approved")
 		@completed_orders = Order.where(status: "completed")
 
 		if @draft_orders.blank?
@@ -10,11 +12,25 @@ class PhysicianController < ApplicationController
 			@draft_orders = get_draft_orders(response1)
 		end
 
-		if @completed_orders.blank?
-			response2 = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/MedicationStatement?patient=1316024&_count=3&status=completed",
+		if @active_orders.blank?
+			response = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/MedicationPrescription?patient=1316034&status=active",
 				{ verify: false,
 					headers: { 'Accept' => 'application/json' } }).parsed_response["entry"]
-			@completed_orders = get_completed_orders(response2)
+			@active_orders = get_active_orders(response)
+		end
+
+		if @approved_orders.blank?
+			response3 = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/MedicationPrescription?patient=1316020&status=completed&_count=5",
+				{ verify: false,
+					headers: { 'Accept' => 'application/json' } }).parsed_response["entry"]
+			@approved_orders = get_approved_orders(response3)
+		end
+
+		if @completed_orders.blank?
+			response4 = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/MedicationStatement?patient=1316024&_count=3&status=completed",
+				{ verify: false,
+					headers: { 'Accept' => 'application/json' } }).parsed_response["entry"]
+			@completed_orders = get_completed_orders(response4)
 		end
 	end
 
@@ -34,6 +50,42 @@ class PhysicianController < ApplicationController
 			draft_order.save
 		end
 		draft_orders
+	end
+
+	private
+	def get_active_orders(response)
+		active_orders = []
+		response.each do |order|
+			patient_id = order["resource"]["patient"]["reference"].split("/").last
+			patient = get_patient(patient_id)
+			order_hash = { patient_name: patient["name"].first["text"],
+				image: patient["photo"].first["data"],
+				order_name: order["resource"]["contained"].first["name"],
+				status: order["resource"]["status"],
+				dosage: order["resource"]["dosageInstruction"].first["text"] }
+			active_order = Order.new(order_hash, without_protection: true)
+			active_orders << active_order
+			active_order.save
+		end
+		active_orders
+	end
+
+	private
+	def get_approved_orders(response)
+		approved_orders = []
+		response.each do |order|
+			patient_id = order["resource"]["patient"]["reference"].split("/").last
+			patient = get_patient(patient_id)
+			order_hash = { patient_name: patient["name"].first["text"],
+				image: patient["photo"].first["data"],
+				order_name: order["resource"]["contained"].first["name"],
+				status: "approved",
+				dosage: order["resource"]["dosageInstruction"].first["text"] }
+			approved_order = Order.new(order_hash, without_protection: true)
+			approved_orders << approved_order
+			approved_order.save
+		end
+		approved_orders
 	end
 
 	private
