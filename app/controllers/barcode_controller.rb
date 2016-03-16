53031e034c41b4d99a454e46eea3b78c8ca76b2c
Regina Ranstrom <reginaranstrom@gmail.com>
2016-03-15 21:40:29 -0400

    barcode scan changes

--- app/controllers/barcode_controller.rb
+++ app/controllers/barcode_controller.rb
@@ -1,12 +1,13 @@
 class BarcodeController < ApplicationController
     def index
     	@order = params[:order]
-  		@care_team = params[:care_team]
-
+    	
   		if @order
   			@patient_name = Order.find(params[:order]).patient_name.split(", ").first
-  		else 
-  			@patient_name = "rheum"
+  		elsif params[:patient_name].empty?
+  			@patient_name = "rheum" 
+  		else
+  			@patient_name = params[:patient_name].split(", ").first
   		end
 
 	  	@response = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/Patient?name=#{@patient_name}&_count=20",
