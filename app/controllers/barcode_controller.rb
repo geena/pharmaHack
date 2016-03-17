ad3c3eeceb2e3a6b3f73dd38dd960bb9c96a46ff
Regina Ranstrom <reginaranstrom@gmail.com>
2016-03-17 18:14:57 -0400

    improved nurse admin page

--- app/controllers/barcode_controller.rb
+++ app/controllers/barcode_controller.rb
@@ -1,13 +1,10 @@
 class BarcodeController < ApplicationController
-    def index
-    	@order = params[:order]
-    	
-  		if @order
-  			@patient_name = Order.find(params[:order]).patient_name.split(", ").first
-  		elsif params[:patient_name].empty?
+    def patient
+  		if params[:patient_name].empty?
   			@patient_name = "rheum" 
   		else
-  			@patient_name = params[:patient_name].split(", ").first
+  			@patient_name = URI.escape(params[:patient_name].strip)
+  			puts @patient_name
   		end
 
 	  	@response = HTTParty.get("https://fhir-open.sandboxcernerpowerchart.com/may2015/d075cf8b-3261-481d-97e5-ba6c48d3b41f/Patient?name=#{@patient_name}&_count=20",
@@ -18,7 +15,7 @@ class BarcodeController < ApplicationController
 	            { verify: false,
 	              headers: { 'Accept' => 'application/json' } }).parsed_response["entry"]
 
-	    @allergies = @allergy_response.map { |entry| entry["resource"]["substance"]["text"] }
+	    @allergies = @allergy_response ? @allergy_response.map { |entry| entry["resource"]["substance"]["text"] } : []
 	    @patient = Patient.new(name: @response["name"].first["text"],
 	                 dob: @response["birthDate"],
 	                 allergies: @allergies)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'barcode/index'
+  get 'barcode/patient'
+
+  get 'barcode/order'
 
   get 'nurse/index'
 
